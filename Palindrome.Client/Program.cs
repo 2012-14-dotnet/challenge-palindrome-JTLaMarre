483db5c3b3ddf18a17597a1742e7154040ca844e
JTLaMarre <lordaufchaos@yahoo.com>
2021-01-04 10:59:20 -0500

    folder struct and UserString obj

---
+++ Palindrome.Client/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Palindrome.Domain;
+
+namespace Palindrome.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           UserString I = new UserString();
+
+           I.GetInput();
+
+           Console.WriteLine(I.Input);
+        }
+    }
+}

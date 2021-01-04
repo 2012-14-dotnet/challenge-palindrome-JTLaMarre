a537522f2d08bfeb44cc1f7edf787dd9221e4e3c
JTLaMarre <lordaufchaos@yahoo.com>
2021-01-04 11:11:09 -0500

    moving logic into a is Pal method

--- Palindrome.Client/Program.cs
+++ Palindrome.Client/Program.cs
@@ -5,13 +5,20 @@ namespace Palindrome.Client
 {
     class Program
     {
+        public static  UserString I = new UserString();
+
+        
+
         static void Main(string[] args)
         {
-           UserString I = new UserString();
-
-           I.GetInput();
-
-           Console.WriteLine(I.Input);
+           IsPal();
+           
+        }
+        public static  void IsPal()
+        {
+            Console.WriteLine("Type a string to see if is palindrome");
+            I.GetInput();
+            
         }
     }
 }

2f21ff49aa470646a9ce2973153bd4be723e18ae
JTLaMarre <lordaufchaos@yahoo.com>
2021-01-04 11:28:24 -0500

    final push

---
+++ Palindrome.Domain/UserString.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Palindrome.Domain
+{
+    public class UserString
+    {
+        public string Input {get;set;}
+
+        public string GetInput(){
+            Input = Console.ReadLine();
+            return Input;
+        }
+    }
+}
